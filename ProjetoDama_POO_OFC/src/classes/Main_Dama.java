9cb2a2c7bb7e97417fc4e3ef8f3128e3a2b07d1b
thevitao <vitorcsantos.pro@gmail.com>
2021-05-02 23:19:06 -0300

    Mudei o Main

--- ProjetoDama_POO_OFC/src/classes/Main_Dama.java
+++ ProjetoDama_POO_OFC/src/classes/Main_Dama.java
@@ -7,7 +7,7 @@ public class Main_Dama {
 		Tabuleiro tabu = new Tabuleiro();
 
 		
-		Peca b1 = new PecaSimples("B1","Branca","42");
+		Peca b1 = new PecaSimples("B1","Branca","41");
 		Peca b2 = new PecaSimples("B2","Branca","41");
 		Peca p1 = new PecaSimples("P1","Preta","15");
 		Peca p2 = new PecaSimples("P2","Preta","14");
@@ -23,7 +23,8 @@ public class Main_Dama {
 		Damas jogo = new Damas(tabu,j1,j2);
 		
 		jogo.imprimir();
-		jogo.jogar(j1, b1, "53");
+		jogo.jogar(j1, b1, "32");
+		jogo.imprimir();
 		
 	}
 
